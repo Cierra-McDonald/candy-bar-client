ed972374226f515774ee5b93194a7d06f8fa6f51
harriet-the-spy <dpcairns@gmail.com>
2021-02-24 12:02:59 -0800

    add list of candies with loading spinner

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
-
+import { getCandies } from './api-utils.js';
+import Spinner from './Spinner.js';
 export default class ListPage extends Component {
+    state = {
+        candies: [],
+        loading: false
+    }
+
+    componentDidMount = async () => {
+        this.setState({ 
+            loading: true,
+         });
+
+        // we need to await anything that returns a Promise
+        const candies = await getCandies();
+
+        this.setState({ 
+            candies: candies,
+            loading: false
+        })
+    }
+
     render() {
         return (
-            <div>
-                list
+            <div className="list">
+                { this.state.loading && <Spinner />}
+                
+                { this.state.candies.map(candy => <div className="candy">
+                    <p>{candy.name}</p>
+                    <p>{candy.category}</p>
+                    <p>{candy.yumminess}</p>
+                    <p>{candy.has_chocolate ? 'it has has chocolate' : 'choco-free'}</p>
+                </div>
+                    )}
             </div>
         )
     }
